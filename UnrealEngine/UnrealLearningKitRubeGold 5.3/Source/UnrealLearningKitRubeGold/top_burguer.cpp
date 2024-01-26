eac2e4e83a4038db11d957d21ecc3f3e5dd87319
Mike <57753913+Terremix@users.noreply.github.com>
2024-01-26 04:26:18 +0100

    ñ

---
+++ UnrealEngine/UnrealLearningKitRubeGold 5.3/Source/UnrealLearningKitRubeGold/top_burguer.cpp
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "top_burguer.h"
+
+// Sets default values
+Atop_burguer::Atop_burguer()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void Atop_burguer::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void Atop_burguer::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
